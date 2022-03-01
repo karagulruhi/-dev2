24ea204591fb8899cd6fe464bfa48519d0d59f13
karagulruhi <79934031+karagulruhi@users.noreply.github.com>
2022-03-01 20:27:26 +0300

    odev 5 6 7

--- odev.py
+++ odev.py
@@ -21,4 +21,65 @@ order by last_name desc
 
 limit 4
 
+odev 6
+
+#soru1
+select avg(rental_rate ) from film
+
+
+
+#soru2
+select count(title) from film
+where title like '%'
+
+#soru3
+select LENGTH from film
+where rental_rate ='0.99'
+Order BY LENGTH DESC 
+LIMIT 1
+
+
+#SORU4
+
+select COUNT(replacement_cost ) from film
+where LENGTH > 150 
+
+
+
+
+ödev 7
+
+#soru1
+
+select * from film
+
+order by rating
+
+
+#soru 2
+
+select replacement_cost,count(*) from film
+
+group by replacement_cost
+
+having count(*)> 50
+
+#soru 3
+
+select store_id, count(*) from customer
+
+group by store_id
+
+
+#soru 4
+select country_id , count(*) from city
+
+group by country_id 
+
+order by count(*) desc
+
+limit 1
+
+
+
 
