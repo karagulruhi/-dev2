4fe15a0e732d8e68629077a9f6bb0e5ddb512946
karagulruhi <Karagulruhii@gmail.com>
2022-03-01 19:24:57 +0300

    odeeev5-6-7-8

---
+++ odev.py
@@ -0,0 +1,24 @@
+ödev 5
+
+
+#soru1
+select * from film
+where title like '%n'
+order by length desc
+limit 5
+
+#soru 2
+select * from film
+where title like '%n'
+order by length desc
+(soruyu anlamadım)
+
+
+#soru 3 
+select * from customer
+where store_id ='1'
+order by last_name desc 
+
+limit 4
+
+
